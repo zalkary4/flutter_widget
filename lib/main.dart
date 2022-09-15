5bf79c681ecf9f9b28fe1dafb52a9dc9dc266d8b
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2022-09-15 23:22:01 +0600

    touch zoom

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget/widgets/gesture_detector_widget.dart';
+import 'package:flutter_widget/widgets/touch_zoom.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const GestureDetectorwidget(),
+      home: const TouchZoom(),
     );
   }
 }
